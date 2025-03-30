9be522eaaa9e26e33100e0a1a50d12fc190c1fc4
tim23333333 <121649367+tim23333333@users.noreply.github.com>
2025-03-30 22:25:12 +0800

    Add files via upload

---
+++ canal_demo02/src/main/java/com/atguigu/canal/CanalDemo02App.java
@@ -0,0 +1,11 @@
+package com.atguigu.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalDemo02App {
+    public static void main(String[] args) {
+        SpringApplication.run(CanalDemo02App.class, args);
+    }
+}
